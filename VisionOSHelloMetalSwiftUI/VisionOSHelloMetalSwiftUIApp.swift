7bedf832b062ceaf098b94af9ab2f0a08429ef24
tokufxug <tokufxug@gmail.com>
2024-08-11 23:30:47 +0900

    Initial Commit

---
+++ VisionOSHelloMetalSwiftUI/VisionOSHelloMetalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSHelloMetalSwiftUIApp.swift
+//  VisionOSHelloMetalSwiftUI
+//
+//  Created by Sadao Tokuyama on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSHelloMetalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOSHelloMetalSwiftUITests/VisionOSHelloMetalSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSHelloMetalSwiftUITests.swift
+//  VisionOSHelloMetalSwiftUITests
+//
+//  Created by Sadao Tokuyama on 8/11/24.
+//
+
+import Testing
+@testable import VisionOSHelloMetalSwiftUI
+
+struct VisionOSHelloMetalSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
